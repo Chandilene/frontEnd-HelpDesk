9f1b27f0be359367a73ae213785ac434f3f72454
Chandilene <chandileneborges@gmail.com>
2026-05-21 15:14:16 -0300

    fix: correção baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.response.use(
